37a9cb335cbbe105b8177c0823ccb9a4536120e0
matthewkeil <me@matthewkeil.com>
2024-05-21 12:19:18 +0200

    feat: update build.rs and Cargo.toml to use hashtree rust bindings

--- build.rs
+++ build.rs
@@ -1,33 +1,26 @@
 extern crate napi_build;
 
-use std::process::Command;
-use std::env;
 use std::path::PathBuf;
+use std::process::Command;
 
 fn main() {
-  let out_dir = env::var("OUT_DIR").expect("OUT_DIR not set");
+  // Define the path to the hashtree bindings directory
+  let hashtree_bindings_dir = PathBuf::from("hashtree/rust_bindings");
 
-  // Define the path to the C dependency source
-  let c_dep_path = "hashtree";
-
-  // Change to the directory containing the C dependency and its Makefile
-  assert!(env::set_current_dir(&PathBuf::from(c_dep_path)).is_ok());
-
-  // make
-  Command::new("make")
-      .status()
-      .unwrap_or_else(|e| panic!("Failed to execute make: {}", e));
-
-  // make install
-  Command::new("make")
-    .arg("install")
-    .arg(format!("DESTDIR={}",out_dir ))
+  // Run the build script of the hashtree library
+  let status = Command::new("cargo")
+    .args(&[
+      "build",
+      "--release",
+      "--manifest-path",
+      hashtree_bindings_dir.join("Cargo.toml").to_str().unwrap(),
+    ])
     .status()
-    .unwrap_or_else(|e| panic!("Failed to execute make install: {}", e));
+    .expect("Failed to build hashtree submodule");
 
-  // Specify the path to the generated library so Rust can link it
-  println!("cargo:rustc-link-search=native={}/usr/lib", out_dir);
-  println!("cargo:rustc-link-lib=static=hashtree");
+  if !status.success() {
+    panic!("Failed to build hashtree submodule");
+  }
 
   // setup napi
   napi_build::setup();
