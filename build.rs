1b4d4f4d66f0586d5c40088f85e01776c2275c4d
matthewkeil <me@matthewkeil.com>
2024-05-27 17:36:25 +0200

    fix: update workflow to not double build deps

--- build.rs
+++ build.rs
@@ -1,29 +1,4 @@
-extern crate napi_build;
-
-use std::path::PathBuf;
-use std::process::Command;
-
 fn main() {
-  // Define the path to the hashtree bindings directory
-  let hashtree_bindings_dir = PathBuf::from("hashtree");
-
-  // Run the build script of the hashtree library
-  let status = Command::new("cargo")
-    .args(&[
-      "build",
-      "--release",
-      "--manifest-path",
-      hashtree_bindings_dir.join("Cargo.toml").to_str().unwrap(),
-    ])
-    .status()
-    .expect("Failed to build hashtree submodule");
-
-  if !status.success() {
-    panic!("Failed to build hashtree submodule");
-  }
-
   // setup napi
   napi_build::setup();
-
-  println!("cargo:rerun-if-changed=build.rs");
 }
